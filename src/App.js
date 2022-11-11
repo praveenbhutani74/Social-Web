5d3900fea90883e1cb20bde49a04603b84991c70
Praveen <bhutanipraveen74@gmail.com>
2022-11-11 16:33:54 +0530

    Routing Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button
+          onClick={() => {
+            this.increment();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Click
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
